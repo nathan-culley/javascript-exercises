c5ec1df34459bf414a5b8faa8ee2b2ecd44db769
Nathan Culley <nathan@nathanculley.com>
2022-06-21 11:55:45 -0400

    Complete first three Odin Project Javascript exercises
    hello world
    repeatString
    reverseString
    
    are all completed with all tests passed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+let string = 'hey';
+let number;
 
-};
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    else {
+        string = string.repeat(num);
+        return string;
+    }
+}
 
+for (number; number < 1000; number++) {
+    repeatString(string,num);
+}
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,25 @@
-const reverseString = function() {
+let input = new String();
+console.log(input);
 
-};
+function reverseString(input) {
+    //break string into an array
+        let forwardArray = new Array();
+        forwardArray = input.split("");
+    //flip array backwards into a new array
+        function flipArray(forwardArray) {
+            let reverse = new Array();
+            for (i = 0; i < forwardArray.length; i++) {
+                reverse[i] = forwardArray[forwardArray.length - i -1];
+            }
+            return reverse;
+        }
+        const reverseArray = flipArray(forwardArray);
+    //re-join array into a new string
+        let newString = reverseArray.join("");
+    //return new backward string
+        return newString;
+}
+reverseString(input);
 
 // Do not edit below this line
 module.exports = reverseString;
