d43dc6df6686f069ffa876c7784f4d98613690b9
Nathan Culley <nathan@nathanculley.com>
2022-06-21 12:51:08 -0400

    complete Exercise 4
    passed all tests for JavaScript exercise 4, 'removeFromArray,' testing a function that removes various values from a given input array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayInput, ...removalArg) {
+    //loop through removalArg
+    for (const arg of removalArg) {
 
-};
+        //loop through arrayInput
+        let i = 0;
+        while (i < arrayInput.length) {
+            //check if any arrayInput element matches removalArg
+            if (arrayInput[i] === arg) {
+                arrayInput.splice(i, 1);
+            }
+            else {
+                i++;
+            }
+        }
+        
+        //END loop through arrayInput
 
+    }
+    //END loop through removalArgs
+    return arrayInput;
+}
+// npm test removeFromArray.spec.js
 // Do not edit below this line
 module.exports = removeFromArray;
