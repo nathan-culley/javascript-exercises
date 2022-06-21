63c1b8ef4a349077ebe5115e14c7948099bbc72b
Nathan Culley <nathan@nathanculley.com>
2022-06-21 14:52:48 -0400

    Complete exercise 6
    Exercise 6 (leapYears) has been completed and passed all tests. The function takes a number as input and checks if it meets the criteria for being a leap year, namely that it is divisible by 4, unless it's divisible by 100, in which case it's not a leap year, EXCEPT in cases when it's divisible by 400, in which case it is. The function uses nested if conditionals to check if the conditions are met and returns 'true' if they are and 'false' if they are not.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
+
+// npm test leapYears.spec.js
 // Do not edit below this line
 module.exports = leapYears;
