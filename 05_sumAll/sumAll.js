291a2854cbc50baa18127700b7f64ba4ed8b11cb
Nathan Culley <nathan@nathanculley.com>
2022-06-21 14:36:29 -0400

    complete Exercise 5
    Exercise 5 (sumAll) has been completed. Created a function that takes two numbers and sums all the numbers between them (including the two numbers); the function returns 'ERROR' if given a negative or non-numeric argument.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (num1 < 0 | num2 < 0) {
+        return "ERROR";
+    }
+    else if (typeof num1 !== 'number' | typeof num2 !== 'number') {
+        return "ERROR";
+    }
+    else if (num2 < num1) {
+        for (let i = num2; i <= num1; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    else {
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    
 };
 
+// npm test sumAll.spec.js
 // Do not edit below this line
 module.exports = sumAll;
