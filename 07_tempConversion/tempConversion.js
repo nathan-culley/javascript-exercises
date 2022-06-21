364d6d64489d6b9e60d62db5ff6cf7abf7b7fd78
Nathan Culley <nathan@nathanculley.com>
2022-06-21 17:45:54 -0400

    Complete exercise 7
    Exercise 7 (tempConversion) has been completed. git clone https://github.com/nathan-culley/JavaScript30.git

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(inputF) {
+  let outputC = (inputF - 32)*(5/9);
+  outputC = +outputC.toFixed(1)
+  return outputC;
 };
 
-const ctof = function() {
-
+const ctof = function(inputC) {
+  let outputF = (inputC * (9/5)) + 32;
+  outputF = +outputF.toFixed(1);
+  return outputF;
 };
+
+
+// npm test tempConversion.spec.js
 
 // Do not edit below this line
 module.exports = {
