2b136c92d4afd12a6883af5b221f0f4db6d6d947
Nathan Culley <nathan@nathanculley.com>
2022-10-25 16:29:59 -0400

    Complete Javascript Exercise 10
    Completed Exercise 10 in the Odin Project's Javascript Exercises. This program generates a fibonacci sequence up to the index provided by the user and returns the last term of that sequence (e.g. index 3 returns term of 2, index 6 returns term of 8, and so on). It converts the user's input to a number and checks if it is positive before continuing. It then enters a simple loop adding prior terms to create the new term for as long as the user's input has not been reached. It returns the variable newTerm to represent the term at the user's chosen index.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+    index = +index;
+    if (index < 1) {
+        return "OOPS";
+    }
+    let firstTerm = 0;
+    //let secondTerm = 1;
+    let newTerm = 1;
+    for (let i = 1; i < index; i++) {
+        newTerm = firstTerm + newTerm;
+        firstTerm = newTerm - firstTerm;
+    }
+    return newTerm;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
