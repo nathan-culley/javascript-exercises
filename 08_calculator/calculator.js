9e5d8d1fb4e40538484d29969baf95bcd38be8a7
Nathan Culley <nathan@nathanculley.com>
2022-10-25 12:50:16 -0400

    Complete Exercise 8
    Created functions to perform all the basic arithmetic operations, including ones involving more than 2 numbers.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let addition = 0;
+  a.forEach(function(addend) {
+    addition = addition + addend;
+  })
+  return addition;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let product = 1;
+  a.forEach(function(multiplier) {
+    product = product * multiplier;
+  })
+  return product;
 };
 
-const power = function() {
-	
+
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let product = 1;
+  for (let i = 1; i <= a; i++) {
+    product = product * i;
+  }
+  return product;
 };
 
 // Do not edit below this line
@@ -31,3 +44,5 @@ module.exports = {
   power,
   factorial
 };
+
+//npm test calculator.spec.js
