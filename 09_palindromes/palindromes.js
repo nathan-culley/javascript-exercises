96b76c747803b93ed1e17852a3c000036c9a0e8f
Nathan Culley <nathan@nathanculley.com>
2022-10-25 15:48:31 -0400

    Complete Javascript Exercise 09
    Created a program that takes in a string and checks if it is a palindrome. Starts by converting the string to lowercase, then removing all non-letter characters (this can easily be adapted to remove only all non-alphanumerical characters by changing the regex used). It takes the result of this process and creates a reversed version of it, then checks if the reverse version and the original version match. If they do, it returns true. If not, it returns false.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    text = text.toLowerCase();
+    let textArray = text.split("");
+    for (let i = textArray.length - 1; i >= 0; i--) {
+        let letterCheck = /^[a-z]+$/;
+        if (textArray[i].match(letterCheck)) {
+            console.log(textArray[i])
+            continue;
+        }
+        else {
+            console.log(textArray[i], ", this is not a letter");
+            textArray.splice(i,1);
+            continue;
+        }
+    }
+    let reverseArray = [];
+    for (let i = 0; i < textArray.length ; i++) {
+        reverseArray.unshift(textArray[i]);
+    }
+    text = textArray.join();
+    let reverse = reverseArray.join();
+    console.log(text, reverse);
+    if (text === reverse) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
+
+//npm test palindromes.spec.js
 // Do not edit below this line
 module.exports = palindromes;
