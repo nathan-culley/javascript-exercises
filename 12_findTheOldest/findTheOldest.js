17e581a242258b366b84fdd831041bac8e2f0ea1
Nathan Culley <nathan@nathanculley.com>
2022-10-26 20:11:05 -0400

    Complete Javascript Exercise 12
    Completed Exercise 12 of the Odin Project's Javascript Exercise. The goal of this exercise was to use array methods to take in any array of objects representing people, with properties of name, yearOfBirth, and yearOfDeath, and return the oldest person from this array. Additional tests involved edge cases such as one or more people still being alive (and therefore having no yearOfDeath). This program first checks to see if the yearOfDeath property is undefined, and if so assigns it the current year with the getFullYear method. After this, it uses a simple inequality check in a sort method to sort the people from oldest to youngest, and returns the first item in the resulting array.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let age = people.sort(function(firstPerson, secondPerson) {
+        if (firstPerson.yearOfDeath == undefined) {
+            let date = new Date();
+            firstPerson.yearOfDeath = date.getFullYear();
+        }
+        if (secondPerson.yearOfDeath == undefined) {
+            let date = new Date();
+            secondPerson.yearOfDeath = date.getFullYear();
+        }
+        if (firstPerson.yearOfDeath - firstPerson.yearOfBirth > secondPerson.yearOfDeath - secondPerson.yearOfBirth) {
+            return -1;
+        }
+        else {
+            return 1;
+        }
+    })
+    return age[0];
 };
 
+//npm test findTheOldest.spec.js
 // Do not edit below this line
 module.exports = findTheOldest;
